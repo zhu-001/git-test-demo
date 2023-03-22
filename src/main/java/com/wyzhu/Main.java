a19d23398a8b748a080b8bfe7f0239e94be9ca7e
wyzhu01 <592368498@qq.com>
2023-03-22 23:51:59 +0800

    hot-fix commit01

--- src/main/java/com/wyzhu/Main.java
+++ src/main/java/com/wyzhu/Main.java
@@ -10,5 +10,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world1!");
         System.out.println("Hello world2!");
+        System.out.println("Hello world3!");
     }
 }
