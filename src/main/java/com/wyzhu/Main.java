8c454fe4328b6b5f2216854113d356c46f252259
wyzhu01 <592368498@qq.com>
2023-03-22 23:43:17 +0800

    first commit

---
+++ src/main/java/com/wyzhu/Main.java
@@ -0,0 +1,12 @@
+package com.wyzhu;
+
+/**
+ * Date:${DATE}
+ * Author:wyzhu
+ * Description:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
