42e598341b1d47837fe1cec482cd530ede414d98
wyzhu01 <592368498@qq.com>
2023-03-22 23:47:38 +0800

    third commit

--- src/main/java/com/wyzhu/Main.java
+++ src/main/java/com/wyzhu/Main.java
@@ -9,5 +9,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello world1!");
+        System.out.println("Hello world2!");
     }
 }
